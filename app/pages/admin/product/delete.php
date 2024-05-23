98219f6c8b60c57f68130ab3798c3a432b12778c
Sumit <sumit@sysqube.com.np>
2024-05-23 18:28:33 +0545

    delete done

---
+++ app/pages/admin/product/delete.php
@@ -0,0 +1,20 @@
+
+<?php
+
+include "../../../connection.php";
+
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Prepare the query to delete the record by ID
+    $query = "DELETE FROM `products` WHERE `product_id` = $id";
+    if (mysqli_query($con, $query)) {
+        $_SESSION['message'] = "Deleted successfully!";
+        header("Location: ./index.php");
+        exit;
+    } else {
+        echo "Error deleting record: " . mysqli_error($con);
+    }
+} else {
+    echo "Invalid ID.";
+}
